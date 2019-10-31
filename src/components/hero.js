e6f536979414169103c27c0b0bedb98530d71180
Knazran <khairulnazran94@gmail.com>
2019-11-01 00:08:29 +0800

    added one more headphone emoji

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Emoji from "./emoji"
 export default function Hero({ props, ...heroProps}) {
   return (
   <div className="py-20 h-screen w-full mb-6" style={{background: "linear-gradient(90deg, #667eea 0%, #764ba2 100%)"}}
@@ -6,7 +7,7 @@ export default function Hero({ props, ...heroProps}) {
   >
     <div className="container mx-auto px-6">
       <h2 className="text-4xl text-center font-bold mb-2 text-white">
-        {heroProps.title}
+        {heroProps.title} 
         </h2>
       <h3 className="text-2xl text-center mb-8 text-gray-200">
         {heroProps.subtitle}
@@ -15,7 +16,8 @@ export default function Hero({ props, ...heroProps}) {
         <div className="max-w-2xl mx-auto rounded overflow-hidden shadow-lg bg-white">
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">
-                    Dengar Lagu Dulu
+                    Dengar Lagu Dulu &nbsp; 
+                    <Emoji symbol="🎧" label="headphone"/>
                 </div>
                 <p className="text-gray-700 text-base">
                     Takde lagu yang layan? Click the Spotify icon and add your songs for yourself and others to layan.
