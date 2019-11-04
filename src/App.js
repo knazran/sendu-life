5cc16ba727232063e6742e3fb04f4eb3329bdf7f
Knazran <khairulnazran94@gmail.com>
2019-11-04 21:16:23 +0800

    added GA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Button from "./components/button";
 import Hero from "./components/hero";
-import Card from "./components/card";
 import TwitterContainer from './components/twitter';
+import ReactGA from 'react-ga';
 
 function App() {
+  initializeReactGA();
   return (
     <div className="flex flex-col w-full my-0">
       <Hero title="Sendu ke?" subtitle="Don't sendu alone my brothers and sisters"/>
@@ -13,6 +13,11 @@ function App() {
     
     
   );
+}
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-151328882-1');
+  ReactGA.pageview('/homepage');
 }
 
 export default App;
